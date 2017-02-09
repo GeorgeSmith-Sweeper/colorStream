43916ea395e6e9dff6ada1f3f928dd48a65cd5d6
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-02-08 21:18:31 -0500

    colors randomly generate on div click

--- scripts.js
+++ scripts.js
@@ -1,24 +1,37 @@
 const main = document.querySelector('.main');
 const button = main.querySelector('.button');
+const colorTester = main.querySelector('.color-tester');
 
 function displayStyles() {
   console.log(`I will eventually display the page styles`);
 }
 
-function createRandomColor() {
+
+// creates random colors in RGB
+function randomColor() {
   let letters = '01234566789ABCDEF';
   let color = '#';
+  let colorArray = ['first', 'second', 'third'];
+
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)]
   }
   return color;
 }
 
+// Applies the random color to the 'body'
+function applyRandomColor() {
+
+  let theGradient = `linear-gradient(135deg, ${randomColor()} 0%, ${randomColor()} 45%, ${randomColor()} 100%)`;
+
+  return document.body.style.background = theGradient;
+}
+
 function changeGradientHourly() {
 
 }
 
-/* revist color flash tomorrow */
+/* unfinished */
 function changeButtonColor() {
   let clicked = false;
   if (clicked) {
@@ -32,3 +45,4 @@ function changeButtonColor() {
 // event listeners
 button.addEventListener('click', displayStyles);
 button.addEventListener('click', changeButtonColor);
+colorTester.addEventListener('click', applyRandomColor);
